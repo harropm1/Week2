48c4ea49eafb20b9c0c2d3b2c2fa46990e261aba
Michelle Harrop <harrop.michelle@gmail.com>
2019-07-30 09:23:45 -0400

    date scripts

---
+++ Mod1/MyWork/DateScripts/MoreDateConversions.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//this creates date objects for various past dates
+
+let dMichelle = new Date("10/11/1989");
+console.log(dMichelle.toLocaleString());
+let dPeggy = new Date("Mar 03 1956");
+console.log(dPeggy.toLocaleString());
+let dBob = new Date(1956, 4, 24);
+console.log(dBob.toLocaleString());
