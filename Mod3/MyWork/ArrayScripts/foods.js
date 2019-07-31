43531b1eb58e60b5eb2c25552e8e1967cafe5fe9
Michelle Harrop <harrop.michelle@gmail.com>
2019-07-31 09:00:03 -0400

    finally finishing the labs

--- Mod3/MyWork/ArrayScripts/foods.js
+++ Mod3/MyWork/ArrayScripts/foods.js
@@ -2,8 +2,36 @@
 
 //making arrays work with two variables in each slot
 
-let lunch = [
+function getBill(lunch)
+{
+    let subtotal = 0;
+
+    for (let i = 0; i < lunch.length; i ++)
+    {
+        subtotal = subtotal + lunch[i].price;
+        console.log(lunch[i].item + " $" + lunch[i].price);
+    }
+    let tax = .08 * subtotal;
+    let tip = .18 * subtotal;
+    let total =  subtotal + tax + tip;
+    
+    let bill = {
+        finalSubtotal: subtotal,
+        finalTax: tax,
+        finalTip: tip,
+        finalTotal: total
+    }
+
+    return bill;
+}
+
+let myLunch = [
     {item: "Steak fajitas", price: 9.95},
     {item: "Chips and Gaucamole", price: 5.25},
     {item: "Sweet Tea", price: 2.79}
 ];
+let bill = getBill(myLunch);
+console.log("\nSubtotal: $" + bill.finalSubtotal.toFixed(2));
+console.log("Tax: $" + bill.finalTax.toFixed(2));
+console.log("Tip: $" + bill.finalTip.toFixed(2));
+console.log("Total: $" + bill.finalTotal.toFixed(2));
